439f5db30995df0a35b28b425169bf9998d697ee
Anton Ageev <tria2006@mail.ru>
2018-07-09 11:49:36 +0300

    added Tariffs controller & view

---
+++ IGSLControlPanel/Controllers/TariffsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IGSLControlPanel.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IGSLControlPanel.Controllers
+{
+    public class TariffsController : Controller
+    {
+        private readonly IGSLContext _context;
+
+        public TariffsController(IGSLContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
