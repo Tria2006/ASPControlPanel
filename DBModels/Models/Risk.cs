02b0f0fc90a8a9d8994dc5f36d16f4db1626228f
Anton Ageev <tria2006@mail.ru>
2018-08-07 11:56:21 +0300

    style changes

--- DBModels/Models/Risk.cs
+++ DBModels/Models/Risk.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using DBModels.Models.ManyToManyLinks;
 
 namespace DBModels.Models
 {
     public class Risk : BaseModel
     {
+        [DisplayName("Значение базового тарифа")]
         public int BaseTariffValue { get; set; }
 
+        [DisplayName("Тип базового тарифа")]
         public int BaseTariffType { get; set; }
 
         public List<RiskInsRuleLink> LinksToInsRules { get; set; } = new List<RiskInsRuleLink>();
--- IGSLControlPanel/Enums/BaseTariffTypes.cs
+++ IGSLControlPanel/Enums/BaseTariffTypes.cs
@@ -1,8 +1,13 @@
-﻿namespace IGSLControlPanel.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IGSLControlPanel.Enums
 {
     public enum BaseTariffTypes
     {
+        [Display(Name = "Сумма в руб.")]
         Amount = 0,
+
+        [Display(Name = "Процент от страховой суммы")]
         Percent = 1
     }
 }
