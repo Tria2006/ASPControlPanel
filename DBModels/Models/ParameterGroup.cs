81e892df036c1f96ef86f86a4756b89492f9f869
Anton Ageev <tria2006@mail.ru>
2018-08-20 10:56:18 +0300

    CanRepeat in Params group

--- DBModels/Models/ParameterGroup.cs
+++ DBModels/Models/ParameterGroup.cs
@@ -1,7 +1,10 @@
-﻿namespace DBModels.Models
+﻿using System.ComponentModel;
+
+namespace DBModels.Models
 {
     public class ParameterGroup : BaseModel
     {
+        [DisplayName("Может повторяться")]
         public bool CanRepeat { get; set; }
     }
 }
