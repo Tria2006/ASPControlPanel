263e0bc8dc597f6f4129d6cd07d4a5d42b62b6b8
Anton Ageev <tria2006@mail.ru>
2018-08-23 16:22:09 +0300

    added unit test (xUnit)

---
+++ IGSLPanelTests/ProductControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DBModels.Models;
+using IGSLControlPanel.Controllers;
+using IGSLControlPanel.Data;
+using IGSLControlPanel.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace IGSLPanelTests
+{
+	public class ProductControllerTests
+	{
+	    private IGSLContext _context;
+
+        public ProductControllerTests()
+		{
+		    var options = new DbContextOptionsBuilder<IGSLContext>()
+		        .UseInMemoryDatabase(Guid.NewGuid().ToString())
+		        .Options;
+		    _context = new IGSLContext(options);
+        }
+
+		[Fact]
+		public void GetIndexViewFromController()
+		{
+            // arrange
+		    var rootFolder = new FolderTreeEntry
+		    {
+		        Name = "RootFolder"
+		    };
+            var folders = new List<FolderTreeEntry> {rootFolder}.AsQueryable();
+            _context.FolderTreeEntries.AddRange(folders);
+		    _context.SaveChanges();
+
+		    var controller = new ProductsController(_context, new FolderDataHelper(), new ProductsHelper(),
+		        new EntityStateHelper(), new HttpContextAccessor());
+
+            // act
+		    var result = controller.Index(rootFolder.Id) as ViewResult;
+
+            // assert
+			Assert.NotNull(result);
+            Assert.Equal(typeof(FolderTreeEntry), result.Model.GetType());
+            Assert.Equal("RootFolder", (result.Model as FolderTreeEntry)?.Name);
+		}
+	}
+}
